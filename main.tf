b570460a346a2d77abd817f88f9aebc4947ac3d3
phanick <39258517+phanick@users.noreply.github.com>
2022-03-25 17:30:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+  # Create AWS ec2 instance
+  resource "aws_instance" "ubuntu-tf-02s" {
+  ami = var.ami_id
+  # vpc_id = "vpc-0043430975cdcbb92"
+  key_name = var.key_name
+  instance_type = var.instance_type
+  # security_groups= var.security_group
+  tags= {
+    Name = var.tag_name
+  }
+}
