968d98dc433a6a3855bbd08389205663f0b9dab5
phanick <39258517+phanick@users.noreply.github.com>
2022-07-08 12:54:06 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "aws-keys-2709"
+  default     =  "key-pair-0807"
 }
 
 variable "instance_type" {
